986cda99bbe1e62e787491be6e483667ec12aceb
William Holmgren <williamdeanholmgren@outlook.com>
2025-02-17 08:59:01 +0000

    Latest changes

--- assets/script.js
+++ assets/script.js
@@ -6,9 +6,13 @@ class PotatoClicker {
 		// Game State
 		this.potatoCount = 0;
 		this.farmerCount = 0;
+		this.mineCount = 0;
 		this.money = 0;
 		this.potatoPrice = 0.55;
 		this.farmerCost = 10;
+		this.mineCost = 100;
+		this.potatoSalesPerSecond = 0;
+
 
 		// Cache DOM Elements
 		this.cacheDOMElements();
@@ -34,6 +38,13 @@ class PotatoClicker {
 		this.buyFarmerBtn = document.getElementById('buyFarmer');
 		this.farmerCountEl = document.getElementById('farmerCount');
 		this.farmerProductionEl = document.getElementById('farmerProduction');
+		this.mineCountEl = document.getElementById('mineCount');
+		this.mineProductionEl = document.getElementById('mineProduction');
+		this.buyMineBtn = document.getElementById('buyMine');
+		this.sellPotatoBtn = document.getElementById('sellPotato');
+		this.potatoSellPriceInput = document.getElementById('potatoSellPriceInput');
+        this.potatoSellPrice = document.getElementById('potatoSellPrice');
+        this.potatoSellRate = document.getElementById('potatoSellRate');
 	}
 
 	/**
@@ -43,6 +54,8 @@ class PotatoClicker {
 		this.createPotatoBtn.addEventListener('click', this.handleCreatePotato.bind(this));
 		this.sellPotatoBtn.addEventListener('click', this.handleSellPotato.bind(this));
 		this.buyFarmerBtn.addEventListener('click', this.handleBuyFarmer.bind(this));
+		this.buyMineBtn.addEventListener('click', this.handleBuyMine.bind(this));
+		this.potatoSellPriceInput.addEventListener('change', this.handleSellPotatoPriceChange.bind(this));
 	}
 
 	/**
@@ -56,6 +69,9 @@ class PotatoClicker {
 		this.farmerCountEl.textContent = this.farmerCount;
 		this.farmerProductionEl.textContent = this.farmerCount+' potato / sec';
 
+		this.mineCountEl.textContent = this.mineCount;
+		this.mineProductionEl.textContent = this.mineCount*5+' potato / sec';
+
 		if (this.potatoCount > 0) {
 			this.sellPotatoBtn.removeAttribute('disabled');
 		} else {
@@ -67,7 +83,29 @@ class PotatoClicker {
 		} else {
 			this.buyFarmerBtn.setAttribute('disabled', 'disabled');
 		}
+
+		if (this.money >= this.mineCost) {
+			this.buyMineBtn.removeAttribute('disabled');
+		} else {
+			this.buyMineBtn.setAttribute('disabled', 'disabled');
+		}
+	
 	}
+	/**
+     * Handler for changing the potato sell price.
+     */
+    handleSellPotatoPriceChange() {
+        const minInput = 0.1;
+        const maxInput = 3.0;
+
+        const potatoPrice = parseFloat(this.potatoSellPriceInput.value);
+        this.potatoSellPrice.textContent = potatoPrice.toFixed(2);
+
+        const clampedInput = Math.min(Math.max(potatoPrice, minInput), maxInput);
+        this.potatoSalesPerSecond = Math.round(((maxInput - clampedInput) / (maxInput - minInput)) * 500);
+        
+        this.potatoSellRate.textContent = this.potatoSalesPerSecond.toFixed(0);
+    }
 
 	/**
 	 * Handler for creating a potato.
@@ -102,12 +140,39 @@ class PotatoClicker {
 	}
 
 	/**
+	 * Handler for buying a mine.
+	 */
+	handleBuyMine() {
+		if (this.money >= this.mineCost) {
+			this.money -= this.mineCost;
+			this.mineCount++;
+		} else {
+			alert('Not enough money to buy a mine!');
+		}
+	}
+
+	/**
 	 * Automatically increment the potato count based on the number of farmers.
 	 */
 	startAutoIncrement() {
 		setInterval(() => {
 			this.potatoCount += this.farmerCount;
+			this.potatoCount += this.mineCount*5;
 		}, 1000);
+		setInterval(() => {
+            if (this.potatoCount === 0) {
+                return;
+            }
+
+            if (this.potatoCount >= this.potatoSalesPerSecond) {
+                this.potatoCount -= this.potatoSalesPerSecond;
+                this.money += this.potatoPrice * this.potatoSalesPerSecond;
+            } else {
+                this.money += this.potatoPrice * this.potatoCount;
+                this.potatoCount = 0;
+            }
+        }, 1000);
+
 	}
 
 	/**
